8ef3da647683067cffb4c0eab82b15be2ef68418
Just Nass <127904963+Nasser-Alzaid@users.noreply.github.com>
2025-03-09 03:35:26 +0300

    sound effect while jumping

--- sense/game_object/main_character.gd
+++ sense/game_object/main_character.gd
@@ -4,15 +4,22 @@ extends CharacterBody2D
 const SPEED = 490.0
 const JUMP_VELOCITY = -990.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $Jump_sound
+
+
 
 
 
 func jump():
 	velocity.y = JUMP_VELOCITY
+	jump_sound.play()
+
+	
 
 func jump_side(x):
 	velocity.y = JUMP_VELOCITY
 	velocity.x = JUMP_VELOCITY
+	
 
 
 func _physics_process(delta: float) -> void:
@@ -29,6 +36,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
